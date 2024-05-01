3a8ad9ae7cb22d09bdf550d35163de0eeca59fa1
Jeremy Kimball <jeremyck54@gmail.com>
2024-05-01 17:11:07 -0600

    Change tab2 name to photos

--- src/App.tsx
+++ src/App.tsx
@@ -72,7 +72,7 @@ const App: React.FC = () => (
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon aria-hidden="true" icon={images} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>Photos</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon aria-hidden="true" icon={square} />
